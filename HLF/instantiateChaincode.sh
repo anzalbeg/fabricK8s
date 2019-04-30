4e6be31d13800bfeb559ea054eeb26bbe7f862ab
Beg <abeg-a@firstam.com>
2019-04-30 17:24:15 +0530

    changed repo name

--- HLF/instantiateChaincode.sh
+++ HLF/instantiateChaincode.sh
@@ -7,12 +7,12 @@ my_dir="$(dirname "$0")"
 
 eval $(parse_yaml fabric-artifacts/values.yaml "config_")
 
-export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricTest/HLF/channel-artifacts/orderer-channel.tx
+export GENESIS_BLOCK=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricK8s/HLF/channel-artifacts/orderer-channel.tx
 export CLI_POD_ID=`kubectl get pod | grep cli | cut -f1 -d' '`
 export ORDERER_ADDR="dev-orderer-g746q:7050"
 export ORG_DOMAIN="org1.example.com"
-export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricTest/HLF/chaincode/chaincode_example02/go
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricTest/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp
+export CHAINCODE_PATH=github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricK8s/HLF/chaincode/chaincode_example02/go
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/crypto-config/fabricK8s/HLF/crypto-config/peerOrganizations/$ORG_DOMAIN/users/Admin@$ORG_DOMAIN/msp
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_ADDRESS="dev-peer0-xyz:7051"
 export CHANNEL_NAME="mychannel"
